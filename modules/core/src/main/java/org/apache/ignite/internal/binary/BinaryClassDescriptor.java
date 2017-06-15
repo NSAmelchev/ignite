1730aaf53976d6ea8fbe3090a8dadec7c1e14430
NSAmelchev <nsamelchev@gmail.com>
2017-06-15 18:14:06 +0300

    Fix. Write as raw.

--- modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/binary/BinaryClassDescriptor.java
@@ -781,6 +781,8 @@ public class BinaryClassDescriptor {
             case EXTERNALIZABLE:
                 if (preWrite(writer, obj)) {
                     try {
+                        writer.rawWriter();
+
                         ((Externalizable)obj).writeExternal(writer);
 
                         postWrite(writer);
